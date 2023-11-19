adce017c2d61861354e1278402bfce566df4f3fc
Aoshima <pazsilvag@gmail.com>
2023-11-19 19:44:38 -0300

    .

--- scripts/blog_name_gen.js
+++ scripts/blog_name_gen.js
@@ -1,4 +1,5 @@
 
+console.log("generating name")
 names = ["tiredness", "possibilites", "realities", "abstractionism"]
 choice1 = Math.random() * names.length()
 choice2 = Math.random() * names.length()
@@ -6,3 +7,5 @@ title = "ramblings of " + names[choice1] + " and " + names[choice2]
 
 document.getElementById("blog-title").innerHTML = title
 document.title = title
+
+console.log(title)
