196a936dc73d95999ac01f10d6809006900d3138
Aoshima <pazsilvag@gmail.com>
2023-11-20 00:04:14 -0300

    .

--- scripts/blog_name_gen.js
+++ scripts/blog_name_gen.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     console.log("generating name")
-    names = ["tiredness", "possibilities", "realities", "abstraction", "lust", "envy", "gluttony", "code", "lasagnas", "joints", "sadness", "wonder", ""]
+    names = ["tiredness", "possibilities", "realities", "abstraction", "lust", "envy", "gluttony", "code", "lasagnas", "joints", "sadness", "wonder", "worlds", "universes", "kings", "kingdoms"]
     choice1 = Math.floor(Math.random()*names.length)
     choice2 = Math.floor(Math.random()*names.length)
     title = "ramblings of " + names[choice1] + " and " + names[choice2]
