645880be4b30bad27e184cc4210058608bc5f4ae
Aoshima <pazsilvag@gmail.com>
2023-11-20 00:55:55 -0300

    .

--- index.md
+++ index.md
@@ -5,5 +5,9 @@ hi! i'm _aoshima_, a gamedev from brazil, and _onwards!_ is my tiny studio.
 
 i make [games](games.html) and [blog](blog.html) about it
 
-you can socially reach me [@itsaoshima](https://twitter.com/aoshimaon){:target="_blank"} and [@onwardsgames](https://twitter.com/onwardsgames){:target="_blank"}<br>
+you can socially reach me [@aoshimaon](https://twitter.com/aoshimaon){:target="_blank"} and [@onwardsgames](https://twitter.com/onwardsgames){:target="_blank"}<br>
 or drop an electronic mail to [aoshima@onwards.games](mailto:aoshima@onwards.games){:target="_blank"}
+
+if you want to keep up with the posts here and new games I might work on, subscribe to the newsletter in the forms at the end of the page
+
+have a nice one!
--- scripts/blog_name_gen.js
+++ scripts/blog_name_gen.js
@@ -1,11 +1,19 @@
 $(document).ready(function() {
-    console.log("generating name")
+    console.log("generating title")
+    
     names = ["tiredness", "possibilities", "realities", "abstraction", "lust", "envy", "gluttony", "code", "lasagnas", "joints", "sadness", "wonder", "worlds", "universes", "kings", "kingdoms"]
+    
     choice1 = Math.floor(Math.random()*names.length)
     choice2 = Math.floor(Math.random()*names.length)
+    
     title = "ramblings of " + names[choice1] + " and " + names[choice2]
 
-    $("#blog-title-header").html(title)
-    $("#blog-title-footer").html(title)
-    document.title = title + " - onwards!"
+    rawTitle = title + "; onwards!"
+    formattedTitle = title + "; <b>onwards!</b>"
+
+    $("#blog-title-header").html(formattedTitle)
+    $("#blog-title-footer").html(formattedTitle)
+    document.title = rawTitle
+
+    console.log("title: " + rawTitle)
 });
